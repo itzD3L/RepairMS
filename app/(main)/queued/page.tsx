93f3405a8f75843ad226c392a705538bdf8aee26
itzD3L <marchuso.deligero@gmail.com>
2026-03-13 18:52:50 +0800

    additional pages

--- app/(main)/queued/page.tsx
+++ app/(main)/queued/page.tsx
@@ -1,7 +1,14 @@
+import { TicketCard } from "@/app/components/ui/tickets/ticketCard";
+import { TicketCardType } from "@/app/lib/definitions";
+import { mockDataTicketCard } from "@/app/lib/mockdata";
+
 export default function Queued() {
     return (
         <div>
             <h1>Queued</h1>
+            {mockDataTicketCard.map((ticket) => (
+                <TicketCard key={ticket.id} ticket={ticket as TicketCardType} />
+            ))}
         </div>
     )
 }
---
+++ app/(new-ticket)/new-ticket/page.tsx
@@ -0,0 +1,7 @@
+export default function NewTicket() {
+    return (
+        <div>
+            <h1>New Ticket</h1>
+        </div>
+    );
+}
